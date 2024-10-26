306290e031bcc02451fa8dbec2808027cf66475a
Aditya <ag7499715142@gmail.com>
2024-10-26 13:39:00 +0530

    Initial COmmit

---
+++ src/middleware/ownershipMiddleware.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const checkOwnership = async (req, res, next) => {
+    const { id } = req.params; // The ID of the dog picture
+    const userId = req.user.id; // Assuming user ID is available in req.user
+    console.log(userId)
+    try {
+        const dogPic = await prisma.dog.findUnique({
+            where: { id: id },
+        });
+
+        if (!dogPic) {
+            // return res.status(404).json({ error: 'Dog picture not found.' });
+            return res.status(404).render('error', { message: 'Dog picture not found' })
+        }
+
+      
+        if (dogPic.userId !== userId) {
+            // return res.status(403).json({ error: 'You are not authorized to perform this action as this image was not uploaded by you.' });
+            return res.status(403).render('error', { message: 'You are not authorized to perform this action as this image was not uploaded by you.' })
+        }
+
+
+        next();
+    } catch (error) {
+        console.error("Error checking ownership:", error);
+        return res.status(500).json({ error: 'An error occurred while checking ownership.' });
+    }
+};
+
+module.exports = checkOwnership;
